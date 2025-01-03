89c2bbcd27b1ecbda2885c16d0e8352b0a8f2a6a
FrotaLucas <lucasdiasfrota@gmail.com>
2025-01-03 23:07:04 +0100

    new layout of msg alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,26 @@ export class AppComponent {
   name: any = ' ';
   vetor = ['Lucas', 'Birgitt', 'Teresa', 'Johanna'];
   showH1: boolean = false;
-  showError: boolean = false;
-
+  // showError: boolean = false;
+  content: string = '';
   add() {
     let n = this.name;
 
     if (n !== ' ') {
       this.vetor.push(n);
+      this.content = 'welcome ' + n + ' =)';
       this.showH1 = true;
-      this.showError = false;
+      // this.showError = false;
       setTimeout(() => {
         this.showH1 = false;
       }, 2000);
     } else {
-      this.showError = true;
+      this.content = 'name required !';
+      // this.showError = true;
+      this.showH1 = true;
+      setTimeout(() => {
+        this.showH1 = false;
+      }, 2000);
     }
   }
 
